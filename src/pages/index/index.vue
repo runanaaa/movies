701b03b20daccb75bb7e694948ac28ded3b08ea2
runanaaa <2267146125@qq.com>
2019-11-22 17:37:44 +0800

    data blinding

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
+   <i-panel :title="title_name">
+    <view style="padding: 15px;">
+      <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+      <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+      <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+         <view slot="content">内容不错</view>
+         <view slot="footer">尾部内容</view>
+      </i-card>
+    </view>
+</i-panel>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      title_name:"推荐"
     }
   },
   methods: {
--- src/pages/movie library/index.vue
+++ src/pages/movie library/index.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
+     <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/1.png" />
+        </i-grid-icon>
+        <i-grid-label>最新</i-grid-label>
+    </i-grid-item>
+   <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/2.png" />
+        </i-grid-icon>
+        <i-grid-label>内地</i-grid-label>
+    </i-grid-item>
+     <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/2.png" />
+        </i-grid-icon>
+        <i-grid-label>中国香港</i-grid-label>
+    </i-grid-item>
+     <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/images/2.png" />
+        </i-grid-icon>
+        <i-grid-label>中国台湾</i-grid-label>
+    </i-grid-item>
+</i-grid>
   </div>
 </template>
 <script>
@@ -15,4 +41,10 @@ export default {
 }
 </script>
 <style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+div >>> .split {
+  margin-bottom: 10pt;
+}
 </style>
