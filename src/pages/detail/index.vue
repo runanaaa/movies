f6a3b3995ff4c820827702075bbf09f6cd9bf59a
runanaaa <2267146125@qq.com>
2019-11-24 18:41:08 +0800

    interface

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div >
+    <i-panel>
+      <view class='header-container'>
+        <image class='bg' :src='option.image'/>
+        <view class='setting'>
+          <image :src='option.image' style="width:250rpx;height:360rpx;"/>
+          <text style="padding:8px;line-height:30px">
+            <text class="movie">{{option.name}}</text>
+            <text class="type">\n类型:{{option.type}}</text>
+            <text class="actor">\n演员:{{option.actor}}</text> 
+          </text>
+        </view>
+      </view>     
+    </i-panel>
+
+    <i-panel style="margin-top:2px">
+     <view  class="li"  @click='changeToggle'>
+        <view  style="padding:8px;line-height:30px"  class='introduce'>剧情简介：{{option.introduce}}</view>
+       
+    </view>
+    </i-panel>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+  },
+   data:{
+    option:[],
+  },
+  onLoad(option){
+   this.option=option;
+   console.log(option)
+  }
+}
+</script>
+<style>
+.li {
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  font-size: 10rpx;
+  width: 100%;
+  padding: 0 4%;
+  height: 500px;
+  line-height: 100rpx;
+  border-bottom: 1rpx solid #f1f1f1;
+}
+.movie{
+  color:white;
+  font-size: 45rpx;
+  font-family:microsoft yahei;
+  font-weight:normal;
+  letter-spacing:1.5px;
+}
+.type{
+  color:#D3D3D3;
+  font-size:32rpx; 
+  font-weight:normal;
+}
+.actor{
+  color:#D3D3D3;
+  font-size:32rpx;
+  font-family:microsoft yahei; 
+}
+.setting{
+  width: 750rpx;
+  height: 450rpx;
+  color: rgb(213, 214, 210);
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 15px;
+}
+.header-container {
+  display: flex;
+  flex-direction: row;
+  height: 450rpx;
+  background-color: #fff;
+  align-items: center;
+  position: relative; 
+}
+.bg{
+filter: blur(9px);
+width: 100%;
+height: 100%;
+}
+.introduce{
+  font-size: 30rpx;
+  color:#696969;
+  margin-left: 20px;
+  margin-right: 20px;
+
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,22 +9,27 @@
     >
     <block v-for="item in imgUrls" :key="item">
       <swiper-item>
-        <image :src="item" style="width:100%;"/>
+        <image :src="item" style="width:100%;" />
       </swiper-item>
     </block>
   </swiper>
-   <i-panel :title="title_name">
-    <view style="padding: 15px;">
-       <i-card  v-for="item in top" :key="item" i-class="split" extra="item.name" :thumb="item.image">
-          <view slot="content">{{item.reason}}</view>
-         <view slot="footer">{{item.type}}</view>
-      </i-card>
-
-    </view>
-</i-panel>
+  <i-panel v-for="item in recommand" :key="item">
+      <view class="setting">
+        <image :src='item.image' style="width:130rpx;height:180rpx;"/>
+        <text style="padding:7px;line-height:17px">
+          <text class="movie">{{item.name}}</text>
+          <text class="type">\n{{item.type}}</text>
+          <text class="actor">\n演员:{{item.actor}}</text> 
+        </text>
+        <button class="button" @click='jump(item)'>查看电影详情</button>
+      </view>
+    </i-panel>
+  
   </div>
 </template>
 <script>
+import card from '@/components/card'
+import top from '@/data/top.json'
 export default {
   data () {
     return {
@@ -44,22 +49,79 @@ export default {
       autoplay: true,
       interval: 5000,
       duration: 1000,
-        top:[
-      {"name":"少年的你","type":"类型：剧情/犯罪/爱情","image":"/static/images/少年的你.jpg","reason":"推荐理由"},
-      {"name":"我和我的祖国","type":"类型：剧情","image":"/static/images/我和我的祖国.jpeg","reason":"推荐理由"},
-      {"name":"中国机长","type":"类型：剧情/灾难","image":"/static/images/中国机长.jpg","reason":"推荐理由"},
-      {"name":"冰雪奇缘2","type":"类型：动画/喜剧/冒险/美国","image":"/static/images/冰雪奇缘2.jpg","reason":"推荐理由"},
-      {"name":"海上钢琴师","type":"类型：剧情/音乐/意大利","image":"/static/images/海上钢琴师.jpg","reason":"推荐理由"},
-    
-      ]
+      recommand:top,
+      components: {
+      card
+       },
+         
     }
   },
   methods: {
+      goToJump(url){
+      wx.navigateTo({url})
     },
+    jump(item){
+    wx.navigateTo({
+       url:'../detail/main?id='+item.id+'&name='+item.name+'&actor='+item.actor+'&type='+item.type+
+       '&image='+item.image+'&introduce='+item.introduce
+    })
+    }
+
+  },
+ 
   created () {
   }
 }
 </script>
 
 <style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+.movie{
+  color:black;
+  font-size: 36rpx;
+  font-family:microsoft yahei;
+  font-weight:normal;
+  letter-spacing:1.5px;
+}
+.type{
+  color:#696969;
+  font-size:28rpx; 
+  font-weight:normal;
+}
+.actor{
+  color:#696969;
+  font-size:28rpx;
+  font-family:microsoft yahei; 
+}
+.button{
+  position: absolute;
+  right: 12px;
+  top: 32px;
+  font-size:14px;
+  background-color:#3bb3e0;
+  color:white;
+  margin:2.5px
+}
+.setting{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left:9px;
+  margin-top: 4px;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
 </style>
